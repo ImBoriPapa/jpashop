84d863bd9d0cd49ec195a070202fca9da1a43fd7
cszzang89@gamail.com <cszzang89@gmail.com>
2022-07-08 00:44:51 +0900

    화면구현 기능구현 완료

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) { // 상품 저장
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId,String name,int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+
+        //save 를 호출할 필요가 없다 변경감지로 업데이트
+
     }
 
     public List<Item> findItems() { // 상품 조회
